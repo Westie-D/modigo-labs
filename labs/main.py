0793198a092229f0031738334606ddab66e924f3
Westie-D <williamwestdamiebi@gmail.com>
2026-09-02 13:37:30 +0100

    Complete: Write a function that takes a list and returns a new list wi

--- labs/main.py
+++ labs/main.py
@@ -1,12 +1,7 @@
-def bmi_report(weight_kg, height_m):
-   bmi = weight_kg / (height_m ** 2)
-   bmi = round(bmi, 1) # TODO: calculate bmi, round it to 1 decimal place, determine the category,
-   if bmi < 18.5:
-      category = "Underweight"
-   elif 18.5 <= bmi <= 24.9:
-      category = "Normal weight"
-   elif 25.0 <= bmi <= 29.9:
-      category = "Overweight"
-   else:
-      category = "Obese"
-   return f"BMI: {bmi}, Category: {category}"
+def remove_duplicates(items):
+    New_list = []
+    
+    for item in items:
+        if item not in New_list:
+            New_list.append(item)# TODO: use a loop to build a new list with duplicates removed, keeping first occurrences
+    return New_list
