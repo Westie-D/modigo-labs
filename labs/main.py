0a29458662e5bddf6859df5c0617eaa15c962fda
Westie-D <williamwestdamiebi@gmail.com>
2026-09-04 13:04:54 +0100

    Complete: A small shop wants to know how many of each item they have i

--- labs/main.py
+++ labs/main.py
@@ -1,14 +1,8 @@
-def fizzbuzz_counts(n):
-    counts = {"fizz": 0, "buzz": 0, "fizzbuzz": 0}
-    
-    for number in range(1, n + 1):
-        if number % 3 == 0 and number % 5 == 0:
-            counts["fizzbuzz"] += 1
-        elif number % 3 == 0:
-            counts["fizz"] += 1
-        elif number % 5 == 0:
-            counts["buzz"] += 1
-        # TODO: check divisibility by 3 and 5, and increment the correct key in `counts`
-        
-    
+def count_items(items):
+    counts = {}# TODO: use a for loop to build a dictionary counting each item in `items`
+    for item in items:
+        if item in counts:
+            counts[item] += 1
+        else:
+            counts[item] = 1
     return counts
