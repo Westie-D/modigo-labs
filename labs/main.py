d0e6e6a65bd6dbdd0b82312cb7a045f76fc80f4d
Westie-D <williamwestdamiebi@gmail.com>
2026-09-09 14:14:24 +0100

    Complete: Write a function that compares two people's hobby sets and r

--- labs/main.py
+++ labs/main.py
@@ -1,4 +1,10 @@
-def merge_tags(tags1, tags2):
-    merged = set(tags1 | tags2)
-
-    return merged
+def compare_hobbies(person1_hobbies, person2_hobbies):
+    return {
+        "shared" : person1_hobbies & person2_hobbies,
+        "only_person1" : person1_hobbies - person2_hobbies,
+        "only_person2" : person2_hobbies - person1_hobbies
+    }
+    result = compare_hobbies(person1_hobbies, person2_hobbies)
+    return result
+    # TODO: use set operations to find shared, only_person1, and only_person2 hobbies
+    pass
