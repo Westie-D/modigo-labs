14d7eef8ba02855d33931083c22cdb6102c828bc
Westie-D <williamwestdamiebi@gmail.com>
2026-09-09 13:31:47 +0100

    Complete: Write a function that takes two coordinate points (as tuples

--- labs/main.py
+++ labs/main.py
@@ -1,13 +1,6 @@
-def list_average(numbers):
-    if len(numbers) == 0:
-        return 0
-
-    total = 0
-    for num in numbers:
-        total = total + num
-
-        average = total / len(numbers)
-    return round(average, 2)
-
-
-        # TODO: use a for loop to calculate the average of `numbers`, rounded to 2 decimal places
+def same_point(point1, point2):
+    # TODO: return True if point1 and point2 represent the same location
+    if point1 == point2:
+        return True 
+    else:
+        return False
